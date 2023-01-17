022c7c63fa13b5a098dfbd1b0a929b4be849ff53
churrowaffles <106877391+churrowaffles@users.noreply.github.com>
2023-01-17 03:35:31 +0000

    reorganise

--- static/index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    // Functions defined in main.js
-    relativeTime($('.latest-table-date'));
-    goToRespective('quotation', $('#quotations tr'))
-    goToRespective('invoice', $('#invoices tr'))
-});
